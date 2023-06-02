82c5147fcc955153610563edba19fd51b152edba
Thanakorn Thanchanangkul <thanakorn.2541@outlook.com>
2023-06-02 11:50:49 +0700

    update Update by for print

--- bbsaha/Models/patient/View_report.cs
+++ bbsaha/Models/patient/View_report.cs
@@ -114,6 +114,6 @@ namespace bbsaha.Models.patient
             public string mchc { get; set; }
             public DateTime CreateDate { get; set; }
             public DateTime UpdateDate { get; set; }
-            public string CreateBy { get; set; }
+            public string Updateby { get; set; }
     }
 }
